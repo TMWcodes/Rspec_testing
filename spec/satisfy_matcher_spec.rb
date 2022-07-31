cec5f38133faa53642314e1ce2cb7333e1b753d1
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-07-31 17:37:27 +0100

    test: satisfy_matcher spec

---
+++ spec/satisfy_matcher_spec.rb
@@ -0,0 +1,13 @@
+
+describe 'satisfy matcher' do
+    subject {'racecar'}
+
+    it 'is a palindrome' do
+        expect(subject).to satisfy { |value| value == value.reverse }
+    end
+    
+    it 'it can take custom assertion message' do
+    expect(subject).to satisfy('be a palindrome') do |value| value == value.reverse 
+        end
+    end
+end
